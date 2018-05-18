56d27d9b77f2407209047d6795002a3c84861132
Jsaon Vranek <jvranek@ucsc.edu>
2018-05-18 01:02:20 -0700

    working tennis ball tracking through filtering and option to use contours to fill in a more perfect circle

--- test_video.py
+++ test_video.py
@@ -10,26 +10,24 @@ import numpy as np
 # lower and upper boundaries of the "green" in HSV
 greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
-#buffer = 64
-# initialize the list of tracked points, to draw the ball trayectory
-#pts = deque(64)
-#resolution=(64,64)255 8 127
-pink = (127, 8, 255)
-# Pinks HSV in CV2 is H=165 S=96.9%, V=100%
-pink_max = np.array([180, 255, 255], np.uint8)
-pink_min = np.array([165, 50, 50], np.uint8)
+
+# thickness of negative one to fill
 THICKNESS = -1
-camera = PiVideoStream().start()
+# the kernel determines the size noise filter
+kernel = np.ones((15,15),np.uint8)
+
+#resolution=(64,64)
+resolution = (320, 240)
+camera = PiVideoStream(resolution=resolution).start()
 time.sleep(2.0)
-i=100000
-while(i):
+while True:
 	frame = camera.read()
 	#frame = imutils.resize(frame, width=400)
 	
 	# resize the frame, blur it, and convert it to the HSV color space
-	#rame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	#frame = imutils.resize(frame, width=600)
+	blurred = cv2.GaussianBlur(frame, (5, 5), 0)
+	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
 	# construct a mask for the color "green", then perform
 	# a series of dilations and erosions to remove any small
@@ -37,13 +35,16 @@ while(i):
 	mask = cv2.inRange(hsv, greenLower, greenUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
+	#remove noise
+	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+	copy = mask.copy()
 	
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)[-2]
 	center = None
- 
+	
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
 		# find the largest contour in the mask, then use
@@ -58,22 +59,18 @@ while(i):
 		if radius > 10:
 			# draw the circle and centroid on the frame,
 			# then update the list of tracked points
+			cv2.circle(mask, (int(x), int(y)), int(radius),
+				255, THICKNESS)
 			cv2.circle(frame, (int(x), int(y)), int(radius),
-				pink, THICKNESS)
+				(0, 255, 255), 2)
 			#cv2.circle(frame, center, 5, (0, 0, 255), -1)
-			
-		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-		mask = cv2.inRange(hsv, pink_min, pink_max)
-		mask = cv2.erode(mask, None, iterations=2)
-		mask = cv2.dilate(mask, None, iterations=2)
 	
-	
-	#cv2.imshow("Frame", frame)
-	cv2.imshow("Mask", mask)
+	cv2.imshow("Frame", frame)
+	cv2.imshow("Circle", mask)
+	cv2.imshow("Filtered", copy)
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord("q"):
 		break
-	i-=1
 camera.stop()
 
 
